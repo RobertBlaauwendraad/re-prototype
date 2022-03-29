8ff96b739ca4dd11c569c18d845d091216a279e4
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2022-03-29 20:29:03 +0200

    Added page titles

--- src/main.js
+++ src/main.js
@@ -1,18 +1,12 @@
 import { createApp } from 'vue'
 import App from "@/App";
-import {createRouter, createWebHistory} from 'vue-router'
-import routes from '@/assets/js/routes';
+import router from "@/assets/js/router.js"
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
 import "@/assets/scss/main.scss"
 import FontAwesomeIcon from "@/assets/js/fontawesome-icons";
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
 
 const app = createApp(App)
 
