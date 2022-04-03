657f5381ee1fc0e3d388f85486f9ac7a5cd301ac
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2022-04-03 13:55:05 +0200

    Added basic express API

---
+++ src/api/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+// Create express app
+const app = express()
+
+// Setup server port
+const port = process.env.PORT || 3000
+
+// Parse requests of content type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+// Parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('Prototype REST API')
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
