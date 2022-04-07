9bd90c3db29a8c0f72e7b0ccbfc7227d584f58a4
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2022-04-07 21:38:25 +0200

    Added db host to env

--- src/api/config/db.config.js
+++ src/api/config/db.config.js
@@ -5,7 +5,7 @@ require('dotenv').config({path: './.env.local'});
 
 // Local mysql db connection
 const dbConnector = mysql.createConnection({
-  host: 'localhost',
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
