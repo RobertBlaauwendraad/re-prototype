a8e009c558f8e012cc2a0ca82a1fab03d3a4a1a3
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2022-04-08 11:18:36 +0200

    Increased axios timeout

--- src/assets/js/axios.js
+++ src/assets/js/axios.js
@@ -8,6 +8,6 @@ const apiUrl = process.env.VUE_APP_API_URL || 'http://localhost:3000';
 
 export default axios.create({
   baseURL: apiUrl + '/api/v1',
-  timeout: 1000,
+  timeout: 30000,
   headers
 });
