806efd482f5281ccb2404870d81c2719543a7c01
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2022-04-08 11:13:36 +0200

    Added example env file

--- src/assets/js/axios.js
+++ src/assets/js/axios.js
@@ -4,8 +4,10 @@ const headers = {
   'Content-Type': 'application/json'
 };
 
+const apiUrl = process.env.VUE_APP_API_URL || 'http://localhost:3000';
+
 export default axios.create({
-  baseURL: process.env.VUE_APP_API_URL + '/api/v1',
+  baseURL: apiUrl + '/api/v1',
   timeout: 1000,
   headers
 });
